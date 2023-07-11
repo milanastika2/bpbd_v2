c85129d4b0cce485e043c13c717e9be7147db46a
I Putu Nikayana <55098903+cungikawake@users.noreply.github.com>
2023-07-11 15:55:00 +0800

    Update details.blade.php

--- resources/views/home/details.blade.php
+++ resources/views/home/details.blade.php
@@ -1,7 +1,7 @@
 @extends('welcome')
 
 @section('latest')
-{!! $seoComment->fbcomment !!}
+{{-- $seoComment->fbcomment --}}
 @foreach($news as $n)
 <div class="single-post">
 	
@@ -35,13 +35,13 @@
 				@endforeach
 			</div>
 		</div><!-- Tags end -->
-
+{{--
     <div class="comments-form">
     <?php  $site = $seoComment->siteurl ?>
             <h3 class="title-normal">Leave a comment</h3>
       <div class="fb-comments" data-href="{{url(''.$site.$n->id)}}" data-numposts="5"></div>
     </div><!-- Comments form end -->
-
+--}}
         
 
 
